ad2c7cb2574f26a31f261da6ccd483544394c476
Søren Lund <slu@mcb.dk>
2016-06-20 12:21:00 +0200

    Added project

---
+++ Calculator.Lib/Calculator.Lib.Test/CalculatorLibTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Calculator.Lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Lib.Test
+{
+    [TestFixture()]
+    public class CalculatorLibTest
+    {
+        private CalculatorLib _calc;
+
+        [SetUp]
+        public void MainSetUp()
+        {
+            _calc = new CalculatorLib();
+        }
+
+        [TestFixture()]
+        public class AddMethod : CalculatorLibTest
+        {
+            [Test]
+            public void Given5and5_ThenReturn10()
+            {
+                var res = _calc.Add(5, 5);
+                Assert.AreEqual(10, res);
+            }
+        }
+
+        [TestFixture()]
+        public class SubtractMethod : CalculatorLibTest
+        {
+            [Test]
+            public void Given5and5_ThenReturn0()
+            {
+                var res = _calc.Subtract(5, 5);
+                Assert.AreEqual(0, res);
+            }
+        }
+    }
+}
---
+++ Calculator.Lib/Calculator.Lib/CalculatorLib.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Lib
+{
+    public class CalculatorLib
+    {
+        public int Add(int a, int b)
+        {
+            var arg1 = a;
+            var arg2 = b; 
+
+            var res = arg1 + arg2;
+            return res;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            var arg1 = a;
+            var arg2 = b;
+
+            var res = arg1 - arg2;
+            return res;
+        }
+    }
+}
